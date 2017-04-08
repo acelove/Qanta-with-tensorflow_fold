93b2d7c04f3ad464ffe8c18845d812c1c2637c71
acelove <chenq47@mail2.sysu.edu.cn>
2017-04-08 10:43:27 +0800

    add metric

--- train.py
+++ train.py
@@ -11,11 +11,13 @@ def root(tree):
 def preprocess(tree_dict, ans_list):
     for key in tree_dict:
         for tree in tree_dict[key]:
-            tree.get(root(tree)).ans = tree.ans_ind
+            #tree.get(root(tree)).ans = tree.ans_ind
             tree.get(root(tree)).dist = tree.dist
             tree.get(root(tree)).qid = tree.qid
-            tree.get(root(tree)).neg = ans_list[ans_list != tree.ans_ind]
+            #tree.get(root(tree)).neg = ans_list[ans_list != tree.ans_ind]
             for node in tree.get_nodes():
+                node.ans = tree.ans_ind
+                node.neg = ans_list[ans_list != tree.ans_ind]
                 new_kids = []
                 for kid in node.kids:
                     new_kids.append((tree.get(kid[0]),kid[1]))
@@ -28,13 +30,10 @@ def get_kids(node):
     return node.kids
 
 def get_ans(node):
-    ans =[]
-    ans.append(node.ans)
-    return ans
+    return node.ans
 
 def get_neg(node):
     neg = []
-    neg.append(node.ans)
     random.shuffle(node.neg)
     neg_list = node.neg
     for i in xrange(100):
@@ -85,13 +84,7 @@ with Wv_mat.scope():
   out = td.Function(tf.matmul).reads(Wv,vec)
   Wv_mat.output.reads(out)
 
-
-expr = td.ForwardDeclaration(td.PyObjectType(),td.TensorType([100,1]))
-kids_deal = ( td.InputTransform(get_kids) >> td.Map(td.Record((expr(), rel2mat)))
-              >> td.Map(Wr_mat) >>td.Fold(td.Function(tf.add),td.FromTensor(tf.zeros((100,1)))) )
-expr_def = td.AllOf(td.InputTransform(word) >> word2vec >> Wv_mat, kids_deal) >>td.Fold(td.Function(tf.add),td.FromTensor(tf.zeros((100,1)))) >>td.Function(tf.tanh)
-expr.resolve_to(expr_def)
-
+loss = td.Composition()
 
 
 expression_label = (
@@ -99,21 +92,41 @@ expression_label = (
                     td.Function(lambda x: tf.nn.embedding_lookup(w2v,x))
                    )
 
+
+with loss.scope():
+  #zero = td.FromTensor(tf.zeros((100,1)))
+  vec0 = loss.input[0]
+  vec1 = loss.input[1] 
+  vec2 = loss.input[2]
+  #vec = td.Function(lambda x:tf.transpose(x,perm=[0,2,1])).reads(vec0)
+  #loss = td.Map(td.Function(lambda x:tf.matmul(x,x,transpose_a=True))).reads(vec2)
+  #loss2 = td.Fold(td.Function(tf.add),td.FromTensor(tf.zeros((100,1)))).reads(loss)
+  loss_pos = td.Function(lambda x,y :1 - tf.matmul(x,y,transpose_a=True)).reads(vec0,vec1) 
+  vec0_b = td.Broadcast().reads(vec0)
+  loss_pos_b = td.Broadcast().reads(loss_pos)
+  loss_neg = (
+               td.Zip() >> td.Map(td.Function(lambda neg,ans,loss_pos : tf.maximum(0.0, tf.matmul(neg,ans,transpose_a=True) + loss_pos)))
+               >> td.Fold(td.Function(tf.add),td.FromTensor(tf.zeros((1,1))))
+             ).reads(vec2,vec0_b,loss_pos_b)
+  td.Metric('loss').reads(loss_neg)
+  loss.output.reads(vec0)
+
+
+
+expr = td.ForwardDeclaration(td.PyObjectType(),td.TensorType([100,1]))
+kids_deal = ( td.InputTransform(get_kids) >> td.Map(td.Record((expr(), rel2mat)))
+              >> td.Map(Wr_mat) >>td.Fold(td.Function(tf.add),td.FromTensor(tf.zeros((100,1)))) )
+pro = td.AllOf(td.InputTransform(word) >> word2vec >> Wv_mat, kids_deal) >>td.Fold(td.Function(tf.add),td.FromTensor(tf.zeros((100,1)))) >>td.Function(tf.tanh) >> td.Function(lambda x:x / tf.norm(x))
+expr_def = td.AllOf(pro,td.InputTransform(get_ans)>>td.Optional(td.Scalar('int32')) >>td.Function(lambda x: tf.nn.embedding_lookup(w2v,x)), td.InputTransform(get_neg) >> td.Map(expression_label)) >>loss
+expr.resolve_to(expr_def)
+
  
-model = td.AllOf(expr_def, td.InputTransform(get_neg) >> td.Map(expression_label)>> td.NGrams(101) >> td.GetItem(0)) 
+model = expr_def
 compiler = td.Compiler.create(model)
-vec = compiler.output_tensors
+vec = compiler.metric_tensors['loss']
 
 
-cos = tf.matmul(vec[0],vec[1],transpose_a=True) / (tf.norm(vec[0],axis=1,keep_dims=True) * tf.norm(vec[1],axis=1,keep_dims=True))
-
-loss_pos = 1 - cos
-loss = loss_pos
-
-for i in xrange(100):
-    loss = loss + tf.maximum(0.0, tf.matmul(vec[0],vec[i+2],transpose_a=True) / (tf.norm(vec[0],axis=1,keep_dims=True) * tf.norm(vec[i+2],axis=1,keep_dims=True)) + loss_pos)
-
-loss = tf.reduce_sum(loss) 
+loss = tf.reduce_sum(vec) 
 
 train_op = tf.train.AdagradOptimizer(0.05).minimize(loss)
 saver = tf.train.Saver()
@@ -132,15 +145,15 @@ sess.run(tf.global_variables_initializer())
 min_loss = 1000000000.0
 for _ in xrange(80):
   loss_sum = 0  
-  for i in xrange(0,len(tree_dict['train']),210):
-    batch = node_dict['train'][i:i+210]
+  for i in xrange(0,len(tree_dict['train']),40):
+    batch = node_dict['train'][i:i+ 40]
     fdict = compiler.build_feed_dict(batch)
     begin = time.time()
     sess.run(train_op,feed_dict=fdict)
     end = time.time()
     loss_batch = sess.run(loss,feed_dict=fdict)
     loss_sum += loss_batch
-    print "epoch :%d ,batch_id : %d ,time cost: %.4f,loss= %.4f" %(_, i/272, end-begin,loss_batch)
+    print "epoch :%d ,batch_id : %d ,time cost: %.4f,loss= %.4f" %(_, i/40, end-begin,loss_batch)
   if loss_sum < min_loss:
     min_loss = loss_sum
   print "epoch loss : %.4f , min loss = %.4f." % (loss_sum, min_loss)
